32bedd1ef2ee9da2ffc4e77f410145a0a42a12d3
BrainlessLabs <brainlesslabs@gmail.com>
2017-03-30 00:11:26 +0530

    Adding singleton library. Adding index.html file.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Polygons
+Ploygon is a 2d game based on shapes.
+
--- backend/main.cpp
+++ backend/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <Box2D/Box2D.h>
+#include <boost/noncopyable.hpp>
 
 int main() {
 
     b2Vec2 grav(0.0, -9.8);
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
